7b3a64d73fbd09c9e3c244048ef7d48a1dac03d6
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 14:41:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,11 +5,14 @@ class Artist
 
   def initialize(name)
     @name = name
-    @@all << self
   end
 
   def add_song(song)
     @songs << song
+  end
+
+  def save
+    @@all << self
   end
 
   def self.all
