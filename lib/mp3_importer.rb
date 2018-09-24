e62f82083f825fcdbf5f2f5a1b568c5132ff4bd5
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 15:09:24 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer
+
+  def initialize(path)
+    @path = path
+  end
+    
+  def files
+
+  end
+
+  def import
+    self.files.each{|filename| Song.new_by_filename(filename)}
+  end
+
+end
