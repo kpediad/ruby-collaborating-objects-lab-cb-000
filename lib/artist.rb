628ded641a304b33aa462e533018b501cfb65f8e
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 14:16:47 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+
+  @@all = []
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.all.find{|artist| artist.name == name} || self.new(name)
+  end
+    
+    
+
+end
